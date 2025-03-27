6cc716f9aba3999efbd0487198956955dce55c8b
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-03-27 12:15:27 +0000

    Initial styling

--- components/SignalDisplay.tsx
+++ components/SignalDisplay.tsx
@@ -5,43 +5,120 @@ import Image from "next/image"
 
 export default function SignalDisplay() {
     return (
-        <Box w="100%" maxW="600px" bg="contentBackground" borderRadius="xl" p={6} zIndex={10}>
+        <Box w="100%" maxW="600px" bg="contentBackground" borderRadius="20px" p={6} zIndex={10}>
             {/* Title and Signal Amount */}
-            <VStack align="stretch" gap={4}>
-                <HStack justify="center" gap={2}>
-                    <Text fontSize="2xl">CSM Signal - Operator 10</Text>
+            <VStack align="stretch" gap={3}>
+                <HStack justify="center" gap={2} w={"100%"} justifyContent={"space-between"} pb={5}>
+                    <HStack bg="blue.800" py={2} pl={2} pr={4} borderRadius="12px">
+                        <Box position="relative" boxSize="40px" borderRadius="full" overflow="hidden">
+                            <Image src="/static/operator-10.png" alt="Operator 10" layout="fill" />
+                        </Box>
+                        <Text fontSize="xl" fontWeight={"bold"}>
+                            Eridian
+                        </Text>
+                    </HStack>
+                    <HStack bg="blue.800" py={2} px={3} borderRadius="12px">
+                        <Text fontSize="xl" fontWeight={"bold"}>
+                            Lido CSM - Operator 10
+                        </Text>
+                    </HStack>
                 </HStack>
 
-                <HStack justify="center" gap={2}>
-                    <Icon as={BsMegaphone} color="yellow.400" />
-                    <Text fontSize="3xl" fontWeight="bold">
-                        7,320.23
+                <HStack
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    gap={5}
+                    fontSize="50px"
+                    fontWeight="bold"
+                    pb={2}
+                >
+                    <Text textAlign={"center"}>High Signal</Text>
+                    <Text px={4} py={0} border={"5px solid"} borderRadius="25px" borderColor="gold.500">
+                        87
                     </Text>
                 </HStack>
 
-                {/* Current Signal Bonus */}
-                <Box bg="green.600" p={2} borderRadius="md" textAlign="center">
-                    <Text>Current Signal Bonus x1.65</Text>
-                </Box>
-
-                {/* Baseline Signal */}
-                <Text color="gray.300">Baseline Signal: 1 per second for being a CSM operator</Text>
+                {/* Current Signal */}
+                <VStack align="stretch" gap={1} pb={8}>
+                    <HStack gap={0} h={"30px"} w={"100%"}>
+                        <Text fontSize="20px" w={"20%"} textAlign="center">
+                            Low
+                        </Text>
+                        <Text fontSize="20px" w={"30%"} textAlign="center">
+                            Mid
+                        </Text>
+                        <Text fontSize="20px" w={"35%"} textAlign="center">
+                            Strong
+                        </Text>
+                        <Text fontSize="20px" w={"15%"} textAlign="center" fontWeight="bold">
+                            High
+                        </Text>
+                    </HStack>
+                    <HStack
+                        gap={0}
+                        h={"35px"}
+                        w={"100%"}
+                        border={"3px solid"}
+                        borderRadius={"10px"}
+                        borderColor={"blue.600"}
+                        overflow={"hidden"}
+                        position="relative"
+                    >
+                        <Box bg="blue.800" w={"87%"} h={"100%"} textAlign="center" />
+                        <Box
+                            position="absolute"
+                            left="20%"
+                            top="0"
+                            bottom="0"
+                            borderLeft="2px dashed"
+                            borderColor="blue.600"
+                        />
+                        <Box
+                            position="absolute"
+                            left="50%"
+                            top="0"
+                            bottom="0"
+                            borderLeft="2px dashed"
+                            borderColor="blue.600"
+                        />
+                        <Box
+                            position="absolute"
+                            left="85%"
+                            top="0"
+                            bottom="0"
+                            borderLeft="2px dashed"
+                            borderColor="blue.600"
+                        />
+                    </HStack>
+                </VStack>
 
                 {/* High Signals Section */}
-                <Text color="red.300" fontSize="xl">
-                    High Signals
+                <Text fontSize="xl" fontWeight={"bold"} pl={3}>
+                    Peak Signals
                 </Text>
 
                 <SimpleGrid columns={3} gap={4}>
-                    <Box bg="blue.800" p={4} borderRadius="md" textAlign="center">
+                    <Box
+                        border={"3px solid"}
+                        borderColor="blue.600"
+                        borderTopRadius="20px"
+                        borderBottomRadius="100px"
+                        textAlign="center"
+                        overflow="hidden"
+                    >
                         <VStack>
-                            <Text fontSize="sm">Testnet Pioneer</Text>
-                            <HStack>
-                                <Box position="relative" boxSize="80px" borderRadius="10px" overflow="hidden">
-                                    <Image src="/static/testnet-pioneer.png" alt="Testnet Pioneer" layout="fill" />
-                                </Box>
-                                <Text color="green.400">+0.1</Text>
-                            </HStack>
+                            <Box position="relative" w="100%" h="0" pb="100%">
+                                <Image
+                                    src="/static/testnet-pioneer.png"
+                                    alt="Testnet Pioneer"
+                                    layout="fill"
+                                    objectFit="cover"
+                                />
+                            </Box>
+                            <Text fontWeight={"bold"}>Testnet Pioneer</Text>
+                            <Text bg="green.500" fontSize="xl" px={2} borderRadius="8px" mb={3}>
+                                +5
+                            </Text>
                         </VStack>
                     </Box>
                     <Box bg="blue.800" p={4} borderRadius="md" textAlign="center">
