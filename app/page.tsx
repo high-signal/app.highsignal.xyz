5191c10e40b2785130e105614b24c1a7a28dab63
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-04-01 12:50:25 +0100

    Add access code on root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import ContentContainer from "../components/layout/ContentContainer"
 import Leaderboard from "../components/Leaderboard"
+import EarlyAccessInput from "../components/EarlyAccessInput"
 
-export default async function Page() {
-    return (
-        <ContentContainer>
-            <Leaderboard />
-        </ContentContainer>
-    )
+interface PageProps {
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default async function Page({ searchParams }: PageProps) {
+    const earlyAccessCode = searchParams.earlyAccessCode
+
+    return <ContentContainer>{earlyAccessCode === "higher" ? <Leaderboard /> : <EarlyAccessInput />}</ContentContainer>
 }
