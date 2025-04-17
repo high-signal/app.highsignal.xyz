1e291d08fa35d73fef4297b49db16bac88553a58
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-04-17 09:16:15 +0100

    Add dev banner

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { HStack, Image, Text, Box } from "@chakra-ui/react"
+import { HStack, Image, Text, Box, VStack } from "@chakra-ui/react"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
@@ -39,34 +39,40 @@ export default function Header({}) {
     const router = useRouter()
 
     return (
-        <HStack
-            w="100%"
-            maxW="1400px"
-            h={"60px"}
-            pt={4}
-            pb={2}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            px={3}
-            zIndex={2}
-        >
-            <Box onClick={() => router.push("/")} cursor="pointer">
-                <HStack gap={2} justifyContent={"center"} alignItems={"center"}>
-                    <Image src="/static/logo/logo-coin.png" alt="Logo" boxSize={"40px"} borderRadius="full" />
-                    <Text minW="80px" fontWeight="bold" fontSize="xl" whiteSpace={"nowrap"}>
-                        {process.env.NEXT_PUBLIC_SITE_NAME}
-                    </Text>
+        <VStack gap={0} w="100%" justifyContent={"center"} alignItems={"center"} zIndex={2}>
+            {process.env.NODE_ENV === "development" && (
+                <HStack w="100%" justifyContent={"center"} alignItems={"center"} bg={"orange.700"}>
+                    <Text>Dev Environment</Text>
                 </HStack>
-            </Box>
-            <HStack gap={{ base: 2, md: 6 }} alignItems={"top"}>
-                {/* <HStack direction="row" wrap="wrap" gap={2} justifyContent="right" pr={{ base: 0, md: 2 }}>
+            )}
+            <HStack
+                w="100%"
+                maxW="1400px"
+                h={"60px"}
+                pt={4}
+                pb={2}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                px={3}
+            >
+                <Box onClick={() => router.push("/")} cursor="pointer">
+                    <HStack gap={2} justifyContent={"center"} alignItems={"center"}>
+                        <Image src="/static/logo/logo-coin.png" alt="Logo" boxSize={"40px"} borderRadius="full" />
+                        <Text minW="80px" fontWeight="bold" fontSize="xl" whiteSpace={"nowrap"}>
+                            {process.env.NEXT_PUBLIC_SITE_NAME}
+                        </Text>
+                    </HStack>
+                </Box>
+                <HStack gap={{ base: 2, md: 6 }} alignItems={"top"}>
+                    {/* <HStack direction="row" wrap="wrap" gap={2} justifyContent="right" pr={{ base: 0, md: 2 }}>
                         {socialLinks.map((link, index) => (
                             <IconLinkButton key={index} {...link} />
                         ))}
                     </HStack> */}
-                {/* <ColorModeToggle /> */}
-                <UserMenuButton />
+                    {/* <ColorModeToggle /> */}
+                    <UserMenuButton />
+                </HStack>
             </HStack>
-        </HStack>
+        </VStack>
     )
 }
