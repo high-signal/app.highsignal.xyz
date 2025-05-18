a500674962738673f308bad754240f409cf20772
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-05-18 21:40:31 +0100

    Style access page

--- components/early-access/EarlyAccessInput.tsx
+++ components/early-access/EarlyAccessInput.tsx
@@ -1,8 +1,10 @@
 "use client"
 
-import { VStack, Input, Button, Text } from "@chakra-ui/react"
+import { VStack, Image, Button, Text, Box, HStack } from "@chakra-ui/react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useState } from "react"
+import SingleLineTextInput from "../ui/SingleLineTextInput"
+import { ASSETS } from "../../config/constants"
 
 export default function EarlyAccessInput() {
     const router = useRouter()
@@ -30,20 +32,30 @@ export default function EarlyAccessInput() {
 
     return (
         <VStack w="100%" pt={50} justifyContent="center" alignItems="center">
-            <VStack gap={6} w="90%" maxW="400px" p={6} mt={5}>
+            <VStack gap={6} w="90%" maxW="300px" p={6} mt={10}>
                 <form onSubmit={handleSubmit} style={{ width: "100%" }}>
-                    <VStack gap={4}>
-                        <Input
+                    <VStack gap={4} transform="scale(1.2)" h={"100%"} minH={"300px"} justifyContent="start">
+                        <Box mb={5}>
+                            <HStack gap={2} justifyContent={"center"} alignItems={"center"}>
+                                <Image
+                                    src={`${ASSETS.LOGO_BASE_URL}/w_300,h_300,c_fill,q_auto,f_webp/${ASSETS.LOGO_ID}`}
+                                    alt="Logo"
+                                    boxSize={"50px"}
+                                    minW={"50px"}
+                                    borderRadius="full"
+                                />
+                                <Text minW="80px" fontWeight="bold" fontSize="xl" whiteSpace={"nowrap"}>
+                                    {process.env.NEXT_PUBLIC_SITE_NAME}
+                                </Text>
+                            </HStack>
+                        </Box>
+                        <SingleLineTextInput
                             placeholder="Enter access code"
                             value={code}
                             onChange={(e) => setCode(e.target.value.toLowerCase())}
                             onKeyDown={handleKeyDown}
-                            size="lg"
-                            bg="gray.700"
-                            border="none"
-                            _focus={{ bg: "gray.700" }}
                         />
-                        <Button size="lg" w="100%" type="submit">
+                        <Button primaryButton borderRadius="full" h={"30px"} w="100%" fontSize={"md"} type="submit">
                             Submit
                         </Button>
                         {error && <Text color="red.400">{error}</Text>}
