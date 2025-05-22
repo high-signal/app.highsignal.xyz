5d149c7d1b504aea37fd78014c0956be66ab2558
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-05-22 16:29:24 +0100

    Style access input

--- components/early-access/EarlyAccessInput.tsx
+++ components/early-access/EarlyAccessInput.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { VStack, Button, Text, HStack, Box, Spinner, Image } from "@chakra-ui/react"
-import { useState } from "react"
+import { VStack, Button, Text, HStack, Box, Spinner, Image, Flex } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 import SingleLineTextInput from "../ui/SingleLineTextInput"
 import { useEarlyAccess } from "../../contexts/EarlyAccessContext"
 import { usePrivy } from "@privy-io/react-auth"
@@ -15,6 +15,12 @@ export default function EarlyAccessInput() {
     const { setHasAccess } = useEarlyAccess()
     const { setTriggerUserCreation } = useUser()
     const { login, logout, authenticated, ready: privyReady } = usePrivy()
+
+    const [isButtonLoading, setIsButtonLoading] = useState(false)
+
+    useEffect(() => {
+        setIsButtonLoading(false)
+    }, [authenticated])
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
@@ -42,8 +48,8 @@ export default function EarlyAccessInput() {
     }
 
     return (
-        <VStack w="100%" pt={50} justifyContent="center" alignItems="center">
-            <HStack gap={2} justifyContent={"center"} alignItems={"center"} cursor={"default"}>
+        <VStack gap={0} w="100%" pt={50} justifyContent="center" alignItems="center" position="relative" zIndex={1}>
+            <HStack gap={2} justifyContent={"center"} alignItems={"center"} cursor={"default"} transform="scale(1.4)">
                 <Image
                     src={`${ASSETS.LOGO_BASE_URL}/w_300,h_300,c_fill,q_auto,f_webp/${ASSETS.LOGO_ID}`}
                     alt="Logo"
@@ -55,11 +61,11 @@ export default function EarlyAccessInput() {
                     {process.env.NEXT_PUBLIC_SITE_NAME}
                 </Text>
             </HStack>
-            <VStack gap={6} w="90%" maxW="300px" px={6}>
+            <VStack gap={6} w="90%" maxW="300px" px={6} transform="scale(1.1)" mt={3}>
                 <form onSubmit={handleSubmit} style={{ width: "100%" }}>
                     <VStack gap={4} h={"100%"} justifyContent="start">
                         {error ? (
-                            <Text color="orange.500" h={"24px"} pt={2}>
+                            <Text color="orange.500" h={"24px"} pt={3}>
                                 {error}
                             </Text>
                         ) : (
@@ -87,22 +93,41 @@ export default function EarlyAccessInput() {
             </VStack>
             {privyReady ? (
                 <>
-                    {!authenticated && <Text>or</Text>}
-                    <HStack gap={2} pt={authenticated ? 3 : 0}>
-                        {authenticated && <Text>Please enter an access code or</Text>}
-                        <Button
-                            primaryButton
-                            borderRadius="full"
-                            overflow="hidden"
-                            justifyContent="center"
-                            h="35px"
-                            w="fit-content"
-                            px={3}
-                            onClick={authenticated ? logout : login}
-                        >
-                            <Text fontWeight={"bold"}>{authenticated ? "Log out" : "Log in"}</Text>
-                        </Button>
-                    </HStack>
+                    <Flex
+                        mt={5}
+                        gap={1}
+                        pt={authenticated ? 3 : 0}
+                        flexWrap={"wrap"}
+                        flexDirection={{ base: "column", md: authenticated ? "row" : "column" }}
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                        transform="scale(1.1)"
+                    >
+                        {authenticated && <Text>Please enter an access code</Text>}
+                        <Text>or</Text>
+                        <Box mt={{ base: 1, md: 0 }} ml={{ base: 0, md: 1 }}>
+                            <Button
+                                primaryButton
+                                borderRadius="full"
+                                overflow="hidden"
+                                justifyContent="center"
+                                h="35px"
+                                w="fit-content"
+                                px={3}
+                                onClick={() => {
+                                    if (authenticated) {
+                                        setIsButtonLoading(true)
+                                        logout()
+                                    } else {
+                                        login()
+                                    }
+                                }}
+                                loading={isButtonLoading}
+                            >
+                                <Text fontWeight={"bold"}>{authenticated ? "Log out" : "Log in"}</Text>
+                            </Button>
+                        </Box>
+                    </Flex>
                 </>
             ) : (
                 <Spinner />
