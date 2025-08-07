246c04988e5c82523055ac888968b380f16523a3
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-08-07 15:37:24 +0100

    Increase Discord queue attempts

--- lambda/scripts/governors/discord/constants.js
+++ lambda/scripts/governors/discord/constants.js
@@ -11,7 +11,7 @@
 const MAX_REQUESTS_PER_SECOND_PER_CHANNEL = 2
 const MAX_QUEUE_LENGTH = 20
 const TIMEOUT_SECONDS = 32 // Lambda timeout is 30 seconds
-const MAX_ATTEMPTS = 2 // Gives it 3 attempts
+const MAX_ATTEMPTS = 10 // Gives it +1 attempts on this number (10 for max queue length of 20 and previous days of 180)
 const MAX_MESSAGES_TO_PROCESS = 100
 const MAX_PAGINATION_LOOPS = 10
 const HEAD_GAP_MINUTES = 60
