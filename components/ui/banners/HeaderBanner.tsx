ff968f24e8a038f4acccbdd119f1c1883ae35736
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-08-15 15:36:36 +0100

    Style close header banner button

--- components/ui/banners/HeaderBanner.tsx
+++ components/ui/banners/HeaderBanner.tsx
@@ -1,14 +1,25 @@
 "use client"
 
 import { VStack, Text } from "@chakra-ui/react"
+import { useState } from "react"
+import ModalCloseButton from "../ModalCloseButton"
 
 export default function HeaderBanner({ banner }: { banner: BannerProps }) {
+    const [isVisible, setIsVisible] = useState(true)
+
+    if (!isVisible) {
+        return null
+    }
+
+    const isClosable = banner.closable
+
     return (
-        <VStack w="100dvw" bg="contentBackground" pt={1} px={3} pb={2} gap={1} zIndex={1} textAlign="center">
-            <Text fontSize="2xl" fontWeight="bold">
+        <VStack w="100dvw" bg="contentBackground" pt={1} pb={2} gap={1} zIndex={1} textAlign="center">
+            <Text fontSize="2xl" fontWeight="bold" px={isClosable ? 10 : 3}>
                 {banner.title}
             </Text>
-            <Text>{banner.content} 123</Text>
+            <Text px={3}>{banner.content}</Text>
+            {isClosable && <ModalCloseButton onClose={() => setIsVisible(false)} noRadius />}
         </VStack>
     )
 }
