cf662caff0c4b1345db65b40e8ce12888d7b7d89
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-08-18 22:51:22 +0100

    Clear lastChecked on error limit reached

--- lambda/scripts/governors/utils/checkQueueForStaleItems.js
+++ lambda/scripts/governors/utils/checkQueueForStaleItems.js
@@ -1,3 +1,5 @@
+const { clearLastChecked } = require("../../engine/utils/lastCheckedUtils")
+
 const checkQueueForStaleItems = async ({ supabase, queueDbTable, MAX_ATTEMPTS, TIMEOUT_SECONDS }) => {
     // ============================
     // Check queue for stale items
@@ -28,6 +30,13 @@ const checkQueueForStaleItems = async ({ supabase, queueDbTable, MAX_ATTEMPTS, T
                 console.error(
                     `‼️ ERROR LIMIT REACHED for queue item ${runningQueueItem.id}. Setting to status "error".`,
                 )
+                await clearLastChecked({
+                    supabase,
+                    userId: runningQueueItem.user_id,
+                    projectId: runningQueueItem.project_id,
+                    signalStrengthId: runningQueueItem.signal_strength_id,
+                })
+
                 const { error: updatedQueueItemError } = await supabase
                     .from(queueDbTable)
                     .update({ status: "error" })
