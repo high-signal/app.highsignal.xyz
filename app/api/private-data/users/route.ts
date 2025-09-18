cd15d184795066863ab34cd3a93611a838e257ef
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-09-18 11:09:33 +0100

    Update comment

--- app/api/private-data/users/route.ts
+++ app/api/private-data/users/route.ts
@@ -1,6 +1,6 @@
 import { getUsersUtil } from "../../../../utils/api-utils/getUsersUtil"
 
-// Protected GET request for logged in users or project admins
+// Protected GET request for logged in users
 // Returns user data for a given project
 export async function GET(request: Request) {
     return getUsersUtil(request, false, true)
