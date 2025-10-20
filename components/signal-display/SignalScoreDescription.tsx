fa921bfa8f74006a9032482b48bd748d5fc92cca
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-10-20 13:19:18 +0100

    Refactor profile rank display

--- components/signal-display/SignalScoreDescription.tsx
+++ components/signal-display/SignalScoreDescription.tsx
@@ -42,34 +42,44 @@ export default function SignalScoreDescription({
 
     if (showLoading) {
         descriptionText = `⏳ Check back later to see ${loggedInUser?.username === currentUser.username ? "your" : "the"} calculated score ⏳`
-    } else if (!isSignalStrengthLoading) {
-        if (currentUser.score === 100) {
-            titleEmoji = "💯"
-            titleText = `${loggedInUser?.username === currentUser.username ? "You have" : `${currentUser.displayName} has`} reached the maximum Signal Score for ${projectData.displayName}`
-            // descriptionText = `Keep engaging with ${projectData.displayName} to maintain this perfect Signal Score!`
-        } else if (currentUser.signal === "high") {
-            titleEmoji = "🤩"
-            titleText = `${loggedInUser?.username === currentUser.username ? "You are" : `${currentUser.displayName} is`} ${currentUser.signal.slice(0, 1).toUpperCase() + currentUser.signal.slice(1)} Signal for ${projectData.displayName}`
-            // descriptionText = `Add more accounts and keep engaging with ${projectData.displayName} to max out your Signal Score!`
-        } else if (currentUser.signal === "mid") {
-            titleEmoji = "🙂"
-            titleText = `${loggedInUser?.username === currentUser.username ? "You are" : `${currentUser.displayName} is`} ${currentUser.signal.slice(0, 1).toUpperCase() + currentUser.signal.slice(1)} Signal for ${projectData.displayName}`
-            // descriptionText = `Add accounts and engage more consistently with ${projectData.displayName} to get a High Signal Score.`
-        } else if (currentUser.signal === "low") {
-            titleEmoji = "😕"
-            titleText = `${loggedInUser?.username === currentUser.username ? "You are" : `${currentUser.displayName} is`} ${currentUser.signal.slice(0, 1).toUpperCase() + currentUser.signal.slice(1)} Signal for ${projectData.displayName}`
-            // descriptionText = `Add more accounts and engage more frequently with ${projectData.displayName} to get a higher Signal Score.`
-        }
     }
+
+    // else if (!isSignalStrengthLoading) {
+    //     if (currentUser.score === 100) {
+    //         titleEmoji = "💯"
+    //         titleText = `${loggedInUser?.username === currentUser.username ? "You have" : `${currentUser.displayName} has`} reached the maximum Signal Score for ${projectData.displayName}`
+    //         // descriptionText = `Keep engaging with ${projectData.displayName} to maintain this perfect Signal Score!`
+    //     } else if (currentUser.signal === "high") {
+    //         titleEmoji = "🤩"
+    //         titleText = `${loggedInUser?.username === currentUser.username ? "You are" : `${currentUser.displayName} is`} ${currentUser.signal.slice(0, 1).toUpperCase() + currentUser.signal.slice(1)} Signal for ${projectData.displayName}`
+    //         // descriptionText = `Add more accounts and keep engaging with ${projectData.displayName} to max out your Signal Score!`
+    //     } else if (currentUser.signal === "mid") {
+    //         titleEmoji = "🙂"
+    //         titleText = `${loggedInUser?.username === currentUser.username ? "You are" : `${currentUser.displayName} is`} ${currentUser.signal.slice(0, 1).toUpperCase() + currentUser.signal.slice(1)} Signal for ${projectData.displayName}`
+    //         // descriptionText = `Add accounts and engage more consistently with ${projectData.displayName} to get a High Signal Score.`
+    //     } else if (currentUser.signal === "low") {
+    //         titleEmoji = "😕"
+    //         titleText = `${loggedInUser?.username === currentUser.username ? "You are" : `${currentUser.displayName} is`} ${currentUser.signal.slice(0, 1).toUpperCase() + currentUser.signal.slice(1)} Signal for ${projectData.displayName}`
+    //         // descriptionText = `Add more accounts and engage more frequently with ${projectData.displayName} to get a higher Signal Score.`
+    //     }
+    // }
 
     if (!titleText && !descriptionText) {
         return null
     }
 
     return (
-        <VStack bg={"contentBackground"} p={4} borderRadius={"16px"} w={"fit-content"} textAlign={"center"} mx={3}>
+        <VStack
+            bg={"contentBackground"}
+            px={4}
+            py={3}
+            borderRadius={"16px"}
+            w={"fit-content"}
+            textAlign={"center"}
+            mx={3}
+        >
             {titleText && (
-                <HStack fontWeight={"bold"} gap={3} alignItems={"start"}>
+                <HStack gap={3} alignItems={"start"}>
                     <Text>{titleEmoji}</Text>
                     <Text>{titleText}</Text>
                     <Text>{titleEmoji}</Text>
--- components/signal-display/UserInfo.tsx
+++ components/signal-display/UserInfo.tsx
@@ -4,15 +4,9 @@ import { VStack, HStack, Text, Box, Image, Button } from "@chakra-ui/react"
 import Link from "next/link"
 import { ASSETS } from "../../config/constants"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { faMedal, faUser } from "@fortawesome/free-solid-svg-icons"
 
-interface UserInfoProps {
-    profileImageUrl: string
-    displayName: string
-    username: string
-}
-
-export default function UserInfo({ profileImageUrl, displayName, username }: UserInfoProps) {
+export default function UserInfo({ currentUser }: { currentUser: UserData }) {
     return (
         <HStack
             gap={3}
@@ -26,22 +20,46 @@ export default function UserInfo({ profileImageUrl, displayName, username }: Use
             justifyContent={"center"}
         >
             <Image
-                src={profileImageUrl || ASSETS.DEFAULT_PROFILE_IMAGE}
-                alt={displayName || ""}
-                boxSize="130px"
+                src={currentUser.profileImageUrl || ASSETS.DEFAULT_PROFILE_IMAGE}
+                alt={currentUser.displayName || ""}
+                boxSize={{ base: "130px", sm: "160px" }}
                 borderRadius="full"
             />
-            <VStack gap={2} alignItems="space-between" justifyContent={"center"} minH={"130px"} px={2} py={2}>
+            <VStack gap={2} alignItems="start" justifyContent={"center"} minH={"130px"} px={2} py={2}>
                 <VStack gap={1} alignItems="start">
-                    <Text fontWeight="bold" fontSize={"30px"} mt={"0px"} wordBreak="break-all" lineHeight="1.2">
-                        {displayName}
+                    <Text
+                        fontWeight="bold"
+                        fontSize="30px"
+                        mt="0px"
+                        wordBreak="break-word" // breaks only at normal word boundaries
+                        overflowWrap="anywhere" // allows breaking inside words when still too long
+                        lineHeight="1.2"
+                    >
+                        {currentUser.displayName || ""}
                     </Text>
                     <Text fontSize="md" color="textColorMuted" mt={"-5px"} pl={"2px"}>
-                        {username}
+                        {currentUser.username || ""}
                     </Text>
                 </VStack>
+                <HStack justifyContent={"center"} pl={1}>
+                    <Box color={"gold.500"}>
+                        <FontAwesomeIcon icon={faMedal} size={"lg"} />
+                    </Box>
+                    <Text>Rank:</Text>
+                    <Text
+                        fontWeight={"bold"}
+                        bg={"pageBackground"}
+                        minW={"40px"}
+                        px={2}
+                        py={1}
+                        borderRadius={"full"}
+                        textAlign={"center"}
+                    >
+                        {currentUser.rank || "-"}
+                    </Text>
+                </HStack>
                 <Box pt={1}>
-                    <Link href={`/u/${username}`}>
+                    <Link href={`/u/${currentUser.username}`}>
                         <Button secondaryButton px={2} py={1} borderRadius="full">
                             <HStack gap={0}>
                                 <FontAwesomeIcon icon={faUser} />
