e5dca33610a9d344fd54e0824bc14392d223d795
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-11-04 17:32:04 +0000

    Reduce max queue length

--- lambda/scripts/governors/ai/constants.js
+++ lambda/scripts/governors/ai/constants.js
@@ -11,7 +11,7 @@
 // Constants
 // ==========
 const MAX_TOKENS_PER_MINUTE = 200000
-const MAX_QUEUE_LENGTH = 200
+const MAX_QUEUE_LENGTH = 20
 const TIMEOUT_SECONDS = 52 // Lambda timeout is 50 seconds
 const MAX_ATTEMPTS = 1 // Gives it 2 attempts
 
